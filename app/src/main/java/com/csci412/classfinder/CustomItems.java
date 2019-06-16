9871ebe9f6df13474162a66d4ea3987d930896ca
Zach Richardson <richarz@wwu.edu>
2019-06-16 15:01:34 -0700

    started json storage

--- app/src/main/java/com/csci412/classfinder/CustomItems.java
+++ app/src/main/java/com/csci412/classfinder/CustomItems.java
@@ -2,7 +2,9 @@ package com.csci412.classfinder;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.DragEvent;
@@ -10,6 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -64,6 +69,140 @@ public class CustomItems {
         SCHEDULES.remove(item);
         SCHEDULE_MAP.remove(item.name);
         rv.getAdapter().notifyDataSetChanged();
+    }
+
+    public static void saveToSharedPrefs(Context context) {
+        SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor prefsEditor = appSharedPrefs.edit();
+        JSONArray json = new JSONArray();
+        for(ScheduleItem schedItem : SCHEDULES) {
+            JSONObject obj = new JSONObject();
+            try {
+                JSONArray ja = new JSONArray();
+                for (Course c: schedItem.classes) {
+                    ja.put(putCourseIntoJSON(c));
+                }
+                obj.put("classes", ja);
+                obj.put("name", schedItem.name);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            json.put(obj);
+        }
+        prefsEditor.putString("json", json.toString());
+        prefsEditor.commit();
+    }
+
+    public static List<ScheduleItem> getFromSharedPrefs(Context context) {
+        SharedPreferences appSharedPrefs = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+        List<ScheduleItem> x = new ArrayList<ScheduleItem>();
+
+        String json = appSharedPrefs.getString("json", "");
+        try {
+            JSONArray ja = new JSONArray(json);
+            int length = ja.length();
+            JSONObject jo;
+            String name;
+            JSONArray classes;
+            JSONObject classO;
+            for (int i = 0; i < length; i++) {
+                jo = ja.getJSONObject(i);
+                classes = jo.getJSONArray("classes");
+                name = jo.getString("name");
+                ScheduleItem item = new ScheduleItem(name);
+                x.add(item);
+                for (int y = 0; y < classes.length(); y++) {
+                    classO = classes.getJSONObject(y);
+                    Course course = getCourseFromJSON(classO);
+                    item.classes.add(course);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return x;
+    }
+
+    public static Course getCourseFromJSON(JSONObject jo) {
+        Course course = new Course();
+        JSONArray ja;
+        try {
+            course.course = jo.getString("course");
+            course.title = jo.getString("title");
+            course.dept = jo.getString("dept");
+            course.crn = jo.getString("crn");
+            course.location = new ArrayList<>();
+            ja = jo.getJSONArray("location");
+            for(int i = 0; i < ja.length(); i++) {
+                course.location.add(ja.get(i).toString());
+            }
+            course.instructor = jo.getString("instructor");
+            course.dates = jo.getString("dates");
+            course.chrgs = jo.getString("chrgs");
+            course.credits = jo.getString("credits");
+            course.attrs = jo.getString("attrs");
+            course.times = new ArrayList<>();
+            ja = jo.getJSONArray("times");
+            for(int i = 0; i < ja.length(); i++) {
+                course.times.add(ja.get(i).toString());
+            }
+            course.prereq = jo.getString("prereq");
+            course.restrictions = jo.getString("restrictions");
+            course.additional = new ArrayList<>();
+            ja = jo.getJSONArray("additional");
+            for(int i = 0; i < ja.length(); i++) {
+                course.additional.add(ja.get(i).toString());
+            }
+            course.waitlist = jo.getBoolean("waitlist");
+            course.cap = jo.getString("cap");
+            course.enrl = jo.getString("enrl");
+            course.avail = jo.getString("avail");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return course;
+    }
+
+    public static String putCourseIntoJSON(Course c) {
+        String json;
+        JSONObject jo = new JSONObject();
+        try {
+            jo.put("course", c.course);
+            jo.put("title", c.title);
+            jo.put("dept", c.dept);
+            jo.put("crn", c.crn);
+            JSONArray location = new JSONArray();
+            for (int i = 0; i < c.location.size(); i++) {
+                location.put(c.location.get(i));
+            }
+            jo.put("location", location);
+            jo.put("instructor", c.instructor);
+            jo.put("dates", c.dates);
+            jo.put("chrgs", c.chrgs);
+            jo.put("credits", c.credits);
+            jo.put("attrs", c.attrs);
+            JSONArray times = new JSONArray();
+            for (int i = 0; i < c.times.size(); i++) {
+                location.put(c.times.get(i));
+            }
+            jo.put("times", times);
+            jo.put("prereq", c.prereq);
+            jo.put("restrictions", c.restrictions);
+            JSONArray additional = new JSONArray();
+            for (int i = 0; i < c.additional.size(); i++) {
+                location.put(c.additional.get(i));
+            }
+            jo.put("additional", additional);
+            jo.put("waitlist", c.waitlist);
+            jo.put("cap", c.cap);
+            jo.put("enrl", c.enrl);
+            jo.put("avail", c.avail);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        json = jo.toString();
+        return json;
     }
 
     public static class SimpleItemRecyclerViewAdapter
